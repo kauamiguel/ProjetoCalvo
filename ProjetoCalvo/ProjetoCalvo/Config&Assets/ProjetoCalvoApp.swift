379866f33d382c49e2daa74f135916e452ab3e83
kauamiguel <kauamcm@gmail.com>
2024-03-25 19:39:39 -0300

    Add initial Project

---
+++ ProjetoCalvo/ProjetoCalvo/Config&Assets/ProjetoCalvoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetoCalvoApp.swift
+//  ProjetoCalvo
+//
+//  Created by Kaua Miguel on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjetoCalvoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            InitialView()
+        }
+    }
+}
---
+++ ProjetoCalvo/ProjetoCalvo/View/InitialView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProjetoCalvo
+//
+//  Created by Kaua Miguel on 25/03/24.
+//
+
+import SwiftUI
+
+struct InitialView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    InitialView()
+}
